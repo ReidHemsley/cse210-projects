341f55e1faaf17eed9b0c697d4629f1972282fdc
ReidHemsley <reidhemsley@gmail.com>
2023-09-13 19:29:53 -0700

    Completed assingment requirements

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string response = Console.ReadLine();
+        int grade = int.Parse(response);
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80 && grade < 90)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            letter = "D";
+        }
+        else if (grade < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+    
+    if (grade < 70)
+    {
+        Console.WriteLine("FAIL. You need a 70% or higher to Pass");
+    }
+    else
+    {
+        Console.WriteLine("PASS");
+    }
     }
 }
